9cba4d76fbba33e764715bfce4b9273f668fbbce
iamaber <abirislam1892@gmail.com>
2025-07-20 17:29:31 +0600

    Fixed main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from automl-ai-assistant-usage-in-student-life!")
+import pickle
+import numpy as np
+import pandas as pd
+
+# Load the trained PyCaret pipeline
+model = pickle.load(open('my_best_pipeline.pkl', 'rb'))
 
 
-if __name__ == "__main__":
-    main()
+sample_data = pd.DataFrame([{
+    'StudentLevel': 'Undergraduate',
+    'Discipline': 'Engineering',
+    'TaskType': 'Assignment',
+    'FinalOutcome': 'Pass',
+    
+}])
+
+# Make prediction
+prediction = model.predict(sample_data)
+print("Prediction:", prediction.values)
